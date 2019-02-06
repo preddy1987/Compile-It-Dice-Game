9b30c725e37609cfaf39880016e115144572b212
patrickreddy <patrickreddy87@gmail.com>
2019-02-06 15:31:23 -0500

    refined beautifcation

--- CompileItCLI/CompileItCLI.cs
+++ CompileItCLI/CompileItCLI.cs
@@ -31,6 +31,7 @@ namespace CompileItCLI
             while (!quitGame)
             {
                 Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("1) Player Management");
                 Console.WriteLine("2) Leader Board");
                 Console.WriteLine("3) Start Game");
@@ -215,6 +216,7 @@ namespace CompileItCLI
         
         private void DisplayPlayerMenu()
         {
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Clear();
             Console.WriteLine("1.) Add Player");
             Console.WriteLine("2.) Remove Player");
@@ -255,13 +257,17 @@ namespace CompileItCLI
                         quit = true;
                     }
                     else
-                    {
+                    {                       
                         DisplayPlayerStatus();
+                        Console.ForegroundColor = ConsoleColor.White;
                         Console.WriteLine();
                         Console.WriteLine("1) Roll");
                         Console.WriteLine("2) End Turn");
                         Console.WriteLine("3) Score Board");
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("4) Suicide");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine();
 
                         int selection = CLIHelper.GetSingleInteger("Select an option...", 1, 4);
 
@@ -362,39 +368,36 @@ namespace CompileItCLI
             string turnSuccesses = $"Turn Successes: {status.TurnSuccesses}";
             string turnWarnings = $"Turn Warnings: {status.TurnWarnings}";
             string odds = $"Odds: {status.Odds.ToString("N2")}";
+            string remainingDie = $"Remaining Dice: {status.RemainingDice.Count}";
+
             Console.WriteLine(header.PadLeft(30, '*'));
             Console.WriteLine($"{header.PadRight(29,' ').PadLeft(26,' ')}*");
             Console.WriteLine($"*{player.PadLeft(20, ' ').PadRight(28,' ')}*");
-            Console.WriteLine($"{header.PadRight(29, ' ').PadLeft(26, ' ')}*");
-            Console.WriteLine(header.PadLeft(30, '*'));
-            ResetColor();
             Console.WriteLine($"*{round.PadLeft(21, ' ').PadRight(28, ' ')}*");
             Console.WriteLine($"*{totalSuccesses.PadLeft(20, ' ').PadRight(28, ' ')}*");
+            Console.WriteLine($"{header.PadRight(29, ' ').PadLeft(26, ' ')}*");
+            Console.WriteLine(header.PadLeft(30, '*'));
+
+            ResetColor();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"{header.PadRight(29, ' ').PadLeft(26, ' ')}*");
             Console.WriteLine($"*{turnErrors.PadLeft(20, ' ').PadRight(28, ' ')}*");
             Console.WriteLine($"*{turnSuccesses.PadLeft(20, ' ').PadRight(28, ' ')}*");
             Console.WriteLine($"*{turnWarnings.PadLeft(20, ' ').PadRight(28, ' ')}*");
-            Console.WriteLine($"*{odds.PadLeft(23, ' ').PadRight(28, ' ')}*");
+            Console.WriteLine($"{header.PadRight(29, ' ').PadLeft(26, ' ')}*");
             Console.WriteLine(header.PadLeft(30, '*'));
 
-            //this was the original code before Beautification
-            //Console.WriteLine(MyString.PadLeft(20, '-'));
-            //Console.WriteLine($"Player: {_game.CurrentPlayerName}");
-            //Console.WriteLine($"Round: {status.RoundCount} {(_game.IsLastRound ? " Last Turn" : "")}");
-            //Console.WriteLine($"Total Successes: {status.TotalSuccesses}");
-            //Console.WriteLine($"Turn Errors: {status.TurnErrors}");
-            //Console.WriteLine($"Turn Successes: {status.TurnSuccesses}");
-            //Console.WriteLine($"Turn Warnings: {status.TurnWarnings}");
-            //Console.WriteLine($"Odds: {status.Odds.ToString("N2")}");
+            Console.WriteLine($"{header.PadRight(29, ' ').PadLeft(26, ' ')}*");
             // Add information about dice in cup
-
+            Console.WriteLine($"*{odds.PadLeft(23, ' ').PadRight(28, ' ')}*");
             //return total amount of die remaining in the cup
-            Console.WriteLine($"Remaining Dice: {status.RemainingDice.Count}");
+            Console.WriteLine($"*{remainingDie.PadLeft(21, ' ').PadRight(28, ' ')}*");
             //color of the remaining die in the cup and quantity
             int greenCounter = 0;
             int redCounter = 0;
             int yellowCounter = 0;
             foreach (var die in status.RemainingDice)
-            {              
+            {
                 if (die.Type == DieType.Green)
                 {
                     greenCounter += 1;
@@ -408,16 +411,41 @@ namespace CompileItCLI
                     yellowCounter += 1;
                 }
             }
+            string greenDie = $"Green: {greenCounter} ";
+            string yellowDie = $"Yellow: {yellowCounter} ";
+            string redDie = $"Red: {redCounter}";
+
             
+            Console.Write("*");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write($"Green: {greenCounter} ");
+            Console.Write(greenDie.PadLeft(10,' '));
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write($"Yellow: {yellowCounter} ");
+            Console.Write(yellowDie);
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write($"Red: {redCounter}");
+            Console.Write(redDie.PadRight(8, ' '));
+            ResetColor();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("*");
+            Console.WriteLine();
+            Console.WriteLine($"{header.PadRight(29, ' ').PadLeft(26, ' ')}*");
+            Console.WriteLine(header.PadLeft(30, '*'));
             Console.WriteLine();
 
+
             ResetColor();
+
+            //this was the original code before Beautification
+            //Console.WriteLine(MyString.PadLeft(20, '-'));
+            //Console.WriteLine($"Player: {_game.CurrentPlayerName}");
+            //Console.WriteLine($"Round: {status.RoundCount} {(_game.IsLastRound ? " Last Turn" : "")}");
+            //Console.WriteLine($"Total Successes: {status.TotalSuccesses}");
+            //Console.WriteLine($"Turn Errors: {status.TurnErrors}");
+            //Console.WriteLine($"Turn Successes: {status.TurnSuccesses}");
+            //Console.WriteLine($"Turn Warnings: {status.TurnWarnings}");
+            //Console.WriteLine($"Odds: {status.Odds.ToString("N2")}");
+
+
+
         }
 
         public Dictionary<int, ConsoleColor> ColorDictionary = new Dictionary<int, ConsoleColor>()
