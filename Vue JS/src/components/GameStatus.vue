ca36b0f0dd5ac053f2acc2a51ead74e87be90540
patrickreddy <patrickreddy87@gmail.com>
2019-04-03 15:23:46 -0400

    made changes to gamestatus

--- Vue JS/src/components/GameStatus.vue
+++ Vue JS/src/components/GameStatus.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="game-status">
-        <h4>Game Status</h4>
-        <ul>
-            <li v-for="player in playersList" :key="player.id" :class="{ current: player.current }">{{player.name}}
-                <span>Score: {{player.score}}</span>
+        <h4>Scoreboard</h4>
+        <ul class="player-container">
+            <li class="player" v-for="player in playersList" :key="player.id" :class="{ current: player.current }">{{player.name}}
+                <span class="round">Round: {{player.round}}</span>
+                <span class="score">Score: {{player.score}}</span>
             </li>
         </ul>
     </div>
@@ -23,7 +24,8 @@ export default {
                         id: i,
                         name: this.players[i].name,
                         current: this.players[i].current,
-                        score: this.players[i].score            
+                        score: this.players[i].score,
+                        round: this.players[i].round            
                     })
             }
             return people;
@@ -40,5 +42,20 @@ div {
 .current{
     font-weight:bold;
     color: seagreen;
+}
+.player-container{
+    display:grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-areas:
+    "player round score"
+}
+.player{
+    grid-area: player;
+}
+.score{
+    grid-area: score;
+}
+.round{
+    grid-area: round;
 }
 </style>
